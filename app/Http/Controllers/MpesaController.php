ae44b4b2828ee8c280c103c4fd8dc42777fc0120
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-12-09 15:42:27 +0300

    Use curl to make http request
    initialize the url
    pars the initialized url to curl_setopt_array
    decode the response and return it

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -8,19 +8,14 @@ class MpesaController extends Controller
 {
     //
     public function getAccessToken(){
-          $url = env('MPESA_ENV') == 0 ? 
-          'https://sandbox.safaricom.co.ke/outh/v1/generate?grant_type=client_credentials' 
-          : '';
-          $curl = curl_init($url);
+          $curl = curl_init('https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials');
           curl_setopt_array(
             $curl,
             array(
-
                 CURLOPT_HTTPHEADER => ['Content-Type:application/json; charset=utf8'],
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_HEADER => false,
                 CURLOPT_USERPWD => env('MPESA_CONSUMER_KEY').':'.env('MPESA_CONSUMER_SECRET'),
-
             )    
           );
           $response = json_decode(curl_exec($curl));
