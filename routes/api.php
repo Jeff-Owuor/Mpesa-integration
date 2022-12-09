0dbfafacdc5783c6bb9935b5256e431871d69088
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-12-09 17:23:32 +0300

    Create post route for validation route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MpesaResponses;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('validation/msape',[MpesaResponses::class,'validation']);
+Route::post('confirmation/msape',[MpesaResponses::class,'confirmation']);
