0cc4c06454ad9571ecc8de763db01a04cf4e4354
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2022-12-15 14:12:57 +0300

    Return the resultcode and resultDec as an array to the log

--- app/Http/Controllers/MpesaResponsesController.php
+++ app/Http/Controllers/MpesaResponsesController.php
@@ -4,16 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
 
 class MpesaResponsesController extends Controller
 {
     //
-    public function validation(Request $request){
+    public function validation(Request $request)
+    {
         Log::info('Validation endpoint hit!');
         Log::info($request->all());
+        
+        return [
+            'ResultCode' => 0,
+            'ResultDec' =>'Accept Service',
+        ];
     }
 
-    public function confirmation(Request $request){
+    public function confirmation(Request $request)
+    {
         Log::info('Confirmation endpoint hit!');
         Log::info($request->all());
     }
